c7612f9c4a69d8fefcd6e1ba13906d6e47a2f4fd
Laura <laurao148@gmail.com>
2018-04-13 04:52:19 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20180413033156) do
 
-  create_table "create_movies", force: :cascade do |t|
+  create_table "movies", force: :cascade do |t|
     t.string  "title"
     t.integer "release_date"
     t.string  "director"
